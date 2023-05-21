dc7e374cc83747a87ea70580f2619d53e66ebdb4
Al.exe <77492646+AlexeyLepov@users.noreply.github.com>
2023-05-21 16:56:13 +0300

    timeout func added

--- test.cpp
+++ test.cpp
@@ -1,88 +1,127 @@
 #include <iostream>
-#include <chrono>
 #include <vector>
+#include <chrono>
+#include <thread>
 #include <fstream>
+#include <string>
 #include <dirent.h>
+
+const int timeoutTime = 1;
 
 
 
 void read(const std::string& filename) 
 {
-    // TODO: Implement your code here
+
+
+    // ||||
+    // VVVV
+    // TODO: Your code for reading files
+    // ...
+
+
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            // Process each line in the file
+            // ...
+        }
+        file.close();
+    } else {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+    }
+}
+
+void find(const std::string& filename)
+{
+
+
+    // ||||
+    // VVVV
+    // TODO: Your algorithm is here
+    // ...
+
+
 }
 
 
 
-void find(const std::string& filename) 
+void findTimeout(const std::string& filename)
 {
-    // TODO: Implement your code here
-    
-    std::cout << "Executing myFunction..." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "myFunction completed." << std::endl;
-}
-
-
-
-// Function with timeout
-bool executeWithTimeout(unsigned int timeoutSeconds)
-{
-    // Create a thread to execute myFunction
-    std::thread t(myFunction);
-
-    // Wait for the specified timeout duration
-    std::this_thread::sleep_for(std::chrono::seconds(timeoutSeconds));
-
-    // Check if the thread is still running
-    if (t.joinable())
+    std::thread t([&]()
     {
-        // Timeout occurred, stop the thread
-        t.detach(); // or t.join();
-        std::cout << "Timeout occurred!" << std::endl;
-        return false;
+        try
+        {
+            find(filename);
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    });
+
+    t.joinable();
+    t.join();
+}
+
+
+
+int main()
+{
+    std::string folderPath = "./tests";
+    std::vector<double> times;
+
+    DIR* dir;
+    struct dirent* entry;
+    if ((dir = opendir(folderPath.c_str())) != nullptr)
+    {
+        while ((entry = readdir(dir)) != nullptr)
+        {
+            std::string filename = entry->d_name;
+            if (filename != "." && filename != "..")
+            {
+                std::string filePath = folderPath + "/" + filename;
+                auto startTime = std::chrono::high_resolution_clock::now();
+
+                try
+                {
+                    findTimeout(filePath);
+                }
+                catch (const std::exception& e)
+                {
+                    std::cerr << e.what() << std::endl;
+                }
+                auto endTime = std::chrono::high_resolution_clock::now();
+                double duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count() / 1'000'000'000.0;
+                times.push_back(duration);
+            }
+        }
+        closedir(dir);
+    }
+    else
+    {
+        std::cerr << "Failed to open directory: " << folderPath << std::endl;
+        return 1;
     }
 
-    // Thread completed within the timeout
-    std::cout << "Execution completed within the timeout." << std::endl;
-    return true;
-}
 
 
-
-int main() {
-    int n = 100;
-    std::vector<std::string> files;
-    DIR* directory = opendir("./tests");
-    struct dirent* entry;
-
-    while ((entry = readdir(directory)) != nullptr)
+    // Printing the result
+    std::string output;
+    for (double time : times)
+    {
+        if (time < timeoutTime)
         {
-#if defined(_DIRENT_HAVE_D_TYPE)
-        if (entry->d_type == DT_REG) { // Check if it is a regular file
-#else
-        // Use platform-specific checks for regular files
-        std::string filePath = std::string("./tests/") + entry->d_name;
-        std::ifstream file(filePath);
-        if (file.good()) { // Check if the file can be opened
-            file.close();
-#endif
-            files.push_back(entry->d_name);
+            output += std::to_string(time) + " ";
+        }
+        else
+        {
+            output += "inf ";
         }
     }
+    output.pop_back();
 
-    closedir(directory);
-
-    for (size_t i = 1; i < files.size(); ++i) 
-    {
-        std::string& file = files[i];
-        auto start = std::chrono::high_resolution_clock::now();
-
-        find(file);
-
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration = end - start;
-        std::cout << duration.count() << std::endl;
-    }
-
+    std::cout << output << std::endl;
     return 0;
 }
